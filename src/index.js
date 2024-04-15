bbb1013bec477b4a55ca9cf6fe1a8558fb261190
Jaques Strydom <jaques.strydom@gmail.com>
2024-04-15 19:02:05 +0200

    Added styling to card

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
