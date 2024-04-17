80e0d3fdd98361d4a6fad0b07063367dd513ffc4
Jaques Strydom <jaques@psi.limited>
2024-04-17 14:42:46 +0200

    Use API to retrieve users

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-import {robots} from "./robots";
+// import {robots} from "./robots";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import './App.css';
+import {robots} from "./robots";
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -21,7 +28,10 @@ class App extends Component {
   render() {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
+    });
+    if (this.state.robots.length === 0) {
+      return <h1>loading!</h1>
+    }
     return (
       <div className="tc">
         <h1 className='f1'>RoboFriends</h1>
