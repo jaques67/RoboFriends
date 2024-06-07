c2504afcfd82b4578fd3480076c9d0b215d28011
Jaques Strydom <jaques.strydom@gmail.com>
2024-06-07 06:48:29 +0200

    Added Redux store to app. Also added middleware to log actions

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,33 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import './App.css';
 import Scroll from "../components/Scroll";
+
+import { setSearchField } from '../actions';
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchField,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+  }
+}
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       robots: [],
-      searchfield: '',
     }
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({robots: users}));
-  }
-
-  onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value});
+      .then(users => this.setState({ robots: users }));
   }
 
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
 
     const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
     if (!robots.length) {
@@ -37,13 +48,13 @@ class App extends Component {
     return (
       <div className="tc">
         <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots}/>
+          <CardList robots={filteredRobots} />
         </Scroll>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
