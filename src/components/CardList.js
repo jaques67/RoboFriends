ec159543fd2d1b9635319995417a7519f1b90d56
DESKTOP-7DB5H5Q\Jaques <jaques@cyberia.technology>
2024-07-01 08:27:46 +0200

    Remove console log statements. Modified package.json

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,10 +2,6 @@ import React from 'react';
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-  // if (true) {
-  //   throw new Error('NOOOO');
-  // }
-  console.log('CardList');
   return (
     <div>
       {robots.map((user, i) => {
